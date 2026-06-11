e8f7abbaeec1f2afbc620975aa2abcc247ca408e
parthmunjal07 <parth.munjal07@gmail.com>
2026-06-11 11:12:28 +0530

    project init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const eslintConfig = defineConfig([
+  eslintPluginPrettierRecommended,
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
